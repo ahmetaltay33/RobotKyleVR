5e3d26c8868fbc9f86b382245b12a1aa56d9e1ae
ahmetaltay33 <altaybilisim@gmail.com>
2021-01-09 22:03:13 +0300

    robot kyle projesinin küçük terrain üzerinde kopyası oluşturuldu.

---
+++ Assets/Scripts/CharacterAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] _forestFootStepClips;
+    [SerializeField]
+    private AudioClip[] _sneakyFootStepClips;
+    [SerializeField]
+    private AudioClip[] _slideClips;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();    
+    }
+
+    private void Step()
+    {
+        var clip = _forestFootStepClips[Random.Range(0, _forestFootStepClips.Length)];
+        _audioSource.PlayOneShot(clip);
+    }
+
+    private void Slide()
+    {
+        var clip = _slideClips[Random.Range(0, _slideClips.Length)];
+        _audioSource.PlayOneShot(clip);
+    }
+
+    private void SneakyStep()
+    {
+        var clip = _sneakyFootStepClips[Random.Range(0, _sneakyFootStepClips.Length)];
+        _audioSource.PlayOneShot(clip);
+    }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        _animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        _animator.SetBool("IsRunning", Input.GetKey("left shift"));
+        _animator.SetBool("IsJumping", Input.GetKey("space"));
+        _animator.SetBool("IsCrouching", Input.GetKey("left ctrl"));
+    }
+}
